d36adcd8776dafe01f78ae3f54d3cb9797cd5cd3
Isaac Wood <isaacw@o-debian.algebra.uni-linz.ac.at>
2025-08-08 10:25:43 +0200

    fixed Rank1Tensor

--- Rank1Tensor.cpp
+++ Rank1Tensor.cpp
@@ -1,50 +1,33 @@
 #include "Rank1Tensor.h"
 
-Rank1Tensor::Rank1Tensor()
-{
-    //ctor
-}
-
-Rank1Tensor::~Rank1Tensor()
-{
-    //dtor
-}
-
-bool Rank1Tensor::update()
-{
-    int i = 0;
-    while (a[i].none()) {
-        i++;
-    }
-    int j = 0;
-    while (b[j].none()) {
-        j++;
-    }
-    int k = 0;
-    while (c[k].none()) {
-        k++;
-    }
-    if (i+j+k+coeff >= MAX_ORDER) {
-        //then we need to remove this term...
-        return true;
-    } else {
-        coeff += i+j+k;
-        //shift the i's j's and k's
-        if (i>0) {
-            for (int ii=0; ii<MAX_ORDER-coeff; ii++) {
-                a[ii] = a[ii+i];
-            }
+std::string Rank1Tensor::to_string() {
+    std::string output = "(";
+    first = true;
+    for (int i=0;i<8;i++) for (int j=0;j<8;j++) {
+        if ((a >> (i*8+j)) & 1UL) {
+            if (first) first = false;
+            else output += "+";
+            output += "a" + std::to_string(i) + std::to_string(j);
         }
-        if (j>0) {
-            for (int ii=0; ii<MAX_ORDER-coeff; ii++) {
-                b[ii] = b[ii+j];
-            }
-        }
-        if (k>0) {
-            for (int ii=0; ii<MAX_ORDER-coeff; ii++) {
-                c[ii] = c[ii+k];
-            }
-        }
-        return false;
     }
+    std::string output = ")(";
+    first = true;
+    for (int i=0;i<8;i++) for (int j=0;j<8;j++) {
+        if ((b >> (i*8+j)) & 1UL) {
+            if (first) first = false;
+            else output += "+";
+            output += "b" + std::to_string(i) + std::to_string(j);
+        }
+    }
+    std::string output = ")(";
+    first = true;
+    for (int i=0;i<8;i++) for (int j=0;j<8;j++) {
+        if ((c >> (i*8+j)) & 1UL) {
+            if (first) first = false;
+            else output += "+";
+            output += "c" + std::to_string(i) + std::to_string(j);
+        }
+    }
+    std::string output = ")";
+    return output;
 }
